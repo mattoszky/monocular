ff011f13a0b5f12adb98edbe4a39ae9a7b2aa2cd
root <root@Mattoszky-PC>
2025-02-07 23:58:17 +0100

    fix Rete.py

--- Rete.py
+++ Rete.py
@@ -10,55 +10,40 @@ class Rete(nn.Module):
         self.layer1 = nn.Sequential(
             nn.Linear(5, 256),
             nn.ReLU(),
-            #nn.Tanh(),
         )
-        """
-        self.layer5 = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            #nn.Tanh(),
-        )
-        """
-        self.layer6 = nn.Sequential(
+        self.layer2 = nn.Sequential(
             nn.Linear(256, 128),
             nn.ReLU(),
-            #nn.Tanh(),
         )
-        self.layer7 = nn.Sequential(
+        self.layer3 = nn.Sequential(
             nn.Linear(128, 64),
             nn.ReLU(),
-            #nn.Tanh(),
         )
-        self.layer8 = nn.Sequential(
+        self.layer4 = nn.Sequential(
             nn.Linear(64, 32),
             nn.ReLU(),
-            #nn.Tanh(),
         )
-        self.layer9 = nn.Sequential(
+        self.layer5 = nn.Sequential(
             nn.Linear(32, 16),
             nn.ReLU(),
-            #nn.Tanh(),
         )
-        self.layer10 = nn.Sequential(
+        self.layer6 = nn.Sequential(
             nn.Linear(16, 8),
             nn.ReLU(),
-            #nn.Tanh(),
         )
-        self.layer11 = nn.Sequential(
+        self.layer7 = nn.Sequential(
             nn.Linear(8, 4),
             nn.ReLU(),
-            #nn.Tanh(),
         )
         self.output = nn.Linear(4, 2) 
 
     def forward(self, x):
         x = self.layer1(x)
-        #x = self.layer5(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
         x = self.layer6(x)
         x = self.layer7(x)
-        x = self.layer8(x)
-        x = self.layer9(x)
-        x = self.layer10(x)
-        x = self.layer11(x)
         x = self.output(x)  # Nessuna funzione di attivazione sull'output per la regressione
         return x
